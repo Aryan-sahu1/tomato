8ac101ad01921c263abbeec46af8d24429b47491
Aryansahu <aryansahuosvn2006@gmail.com>
2024-09-01 03:26:00 +0530

    first-day-work

---
+++ src/components/Navbar/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <>
+      <div className="header">
+        <div className="header-content">
+            <h2>
+                Order Your Favorite Food
+            </h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione eius veniam enim corrupti ipsa aspernatur, recusandae, perspiciatis nihil, vero numquam possimus eligendi vel quidem maxime quod quasi corporis explicabo officia?</p>
+            <button>View Menu</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets/assets'
 const Navbar = () => {
-    const [menu,setMenu]= useState("menu");
+    const [menu,setMenu]= useState("home");
 
 
     return (
@@ -17,7 +17,7 @@ const Navbar = () => {
                 </ul>
                 <div className="navbar-right">
                     <img src={assets.search_icon} alt="" />
-                    <div className="navbar-search0icon">
+                    <div className="navbar-search-icon">
                         <img src={assets.basket_icon} alt="" />
                         <div className="dot"></div>
                     </div>
