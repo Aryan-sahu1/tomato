b6998e4fbf7ae75324a4d5c709d1fab8cf3be0e3
Aryansahu <aryansahuosvn2006@gmail.com>
2024-09-01 06:38:38 +0530

    addng media quary

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
           <Route path='/order' element={<PlaceOrder />} />
         </Routes>
       </div>
+      <Footer/>
     </Router>
   );
 }
